f0b0d5cf967db69bcfba8912e443c062830a07c6
gusprada <peepx339@gmail.com>
2025-03-15 02:37:11 +0700

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 function checkResults() {
-    const resultContainer = document.getElementById("result-container");
-    const resultPopup = document.getElementById("result-popup");
-    const resultMessage = document.getElementById("result");
-    const winningNumber = Math.floor(100000 + Math.random() * 900000); // Random winning number
     const lotto = document.getElementById("lotto").value.trim();
-
+    const resultMessage = document.getElementById("resultMessage");
+    const resultModal = new bootstrap.Modal(document.getElementById('resultModal'));
+    
     if (lotto.length !== 6) {
         resultMessage.innerHTML = "กรุณากรอกเลขสลาก 6 หลัก";
-        openPopup();
+        resultModal.show();
         return;
     }
 
+    const winningNumber = Math.floor(100000 + Math.random() * 900000); // Random winning number
     if (lotto === winningNumber.toString()) {
         resultMessage.innerHTML = `ยินดีด้วย! เลขที่คุณกรอกตรงกับรางวัล 🎉<br>เลขที่ออก: ${winningNumber}`;
     } else {
         resultMessage.innerHTML = `น่าเสียดาย... เลขที่คุณกรอกไม่ตรงกับรางวัล 😢<br>เลขที่ออก: ${winningNumber}`;
     }
 
-    openPopup();
-}
-
-function openPopup() {
-    const resultContainer = document.getElementById("result-container");
-    resultContainer.style.display = "flex";
-}
-
-function closePopup() {
-    const resultContainer = document.getElementById("result-container");
-    resultContainer.style.display = "none";
+    resultModal.show();
 }
 
 function resetForm() {
     document.getElementById("lotto").value = '';
-    document.getElementById("result").innerHTML = '';
+    document.getElementById("resultMessage").innerHTML = '';
 }
